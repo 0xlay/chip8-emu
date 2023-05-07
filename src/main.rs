87b7c2ffb54ac5ccc38b91704bb1a899409a2b34
0xlay <zeroxlay@protonmail.com>
2023-05-07 10:51:59 +0300

    feat: deploy chip8 in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod chip8;
+mod display;
+mod instructions;
+mod keyboard;
+mod ram;
+mod registers;
+
+use chip8::Chip8;
+
+// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+// TODO: complete the implementation of the Chip8 emulator.
+// TODO: Отрисовать окно или с помощью вулкан или opengl
+// https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+    if args.len() == 1 {
+        let mut chip8 = Chip8::new();
+        if let Err(err) = chip8.load_rom(&args[0]) {
+            eprintln!("CHIP8 load rom: {err}");
+        } else if let Err(err) = chip8.run() {
+            eprintln!("CHIP8 run: {err}");
+        }
+    } else {
+        eprintln!("CHIP8: Invalid arguments!")
+    }
 }
